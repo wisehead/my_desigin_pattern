3e70c13b57525b998f57a1a74988716c6da3010d
wisehead <alex.chenhui@gmail.com>
2020-02-29 22:01:47 +0800

    27.visitor

---
+++ 27.visitor/code/ShoppingCart.h
@@ -0,0 +1,26 @@
+#ifndef __SHOPPINGCART_H__
+#define __SHOPPINGCART_H__
+
+#include "Element.h"
+#include "Visitor.h"
+#include <vector>
+
+class ShoppingCart
+{
+public:
+	ShoppingCart(){}
+	void addElement(Element* element){
+		//printf("  商品名：%s, \t数量：%d, \t加入购物车成功！\n", element->getName().c_str(), element->getNum());
+		printf("  product name:%s, \tnum:%d, \tadd to shopping cart successfully!!\n", element->getName().c_str(), element->getNum());
+		elementList.push_back(element);
+	}
+	void accept(Visitor* visitor){
+		for (int i = 0; i < elementList.size(); i++){
+			elementList[i]->accept(visitor);
+		}
+	}
+private:
+	vector<Element*>elementList;
+};
+
+#endif 
