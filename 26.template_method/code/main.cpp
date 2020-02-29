b26fa79ff21230928ddab0beee1ec60daf12301e
wisehead <alex.chenhui@gmail.com>
2020-02-29 21:34:37 +0800

    26.template_method

---
+++ 26.template_method/code/main.cpp
@@ -0,0 +1,20 @@
+#include "FingerprintModule.h"
+#include <iostream>
+//#include <Windows.h>
+
+int main()
+{
+	FingerprintModule *fp = new FingerprintModuleA();
+	fp->algorithm();
+
+	fp = new FingerprintModuleB();
+	fp->algorithm();
+
+	fp = new FingerprintModuleC();
+	fp->algorithm();
+
+
+	printf("\n\n");
+	system("pause");
+	return 0;
+}
